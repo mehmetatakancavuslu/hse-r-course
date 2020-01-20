3c67c725f5c2fd3ab557ee0de31d131c79c44fb9
atakancavuslu <mcavuslu15@ku.edu.tr>
2020-01-20 19:02:47 +0300

    Review plots

--- ClassDay2.R
+++ ClassDay2.R
@@ -19,3 +19,30 @@ agg.data[2,]
 table(seg.df$Segment, seg.df$ownHome)
 # Another way to write it with()
 with(seg.df, table(Segment, ownHome))
+# Prop.table to get the proportions
+with(seg.df, prop.table(table(Segment, ownHome)))
+with(seg.df, prop.table(table(Segment, ownHome), margin = 1))
+# Doing math in a table, adding up the total number of kids in each segment
+aggregate(kids ~ Segment, data = seg.df, sum)
+
+# Histograms - Example of lattice package
+library(lattice)
+histogram(~subscribe | Segment, data = seg.df)
+histogram(~subscribe | Segment, data = seg.df, type = "count", layout = c(4,1),
+          col = c("burlywood", "darkolivegreen"))
+# Histograms by two factors
+histogram(~subscribe | Segment + ownHome, data = seg.df)
+
+# Aggregate continuous data
+seg.mean <- aggregate(income ~ Segment, data = seg.df, mean)
+barchart(income ~ Segment, data = seg.mean, col = "grey")
+# Continuous data by two factors
+seg.agg <- aggregate(income ~ Segment + ownHome, data = seg.df, mean)
+barchart(income ~ Segment, data = seg.agg, groups = ownHome, auto.key = TRUE,
+         par.settings = simpleTheme(col = c("gray95", "gray50")))
+
+# Boxplots - with lattice
+bwplot(Segment ~ income, data = seg.df, horizontal = TRUE, xlab = "Income")
+# Boxplots with two grouping
+bwplot(Segment ~ income | ownHome, data = seg.df,
+       horizontal = TRUE, xlab = "Income")
