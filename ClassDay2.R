90ed56fde2fd85512297eb38399449aa567baea1
atakancavuslu <mcavuslu15@ku.edu.tr>
2020-01-20 20:49:01 +0300

    Finalize day 2

--- ClassDay2.R
+++ ClassDay2.R
@@ -46,3 +46,42 @@ bwplot(Segment ~ income, data = seg.df, horizontal = TRUE, xlab = "Income")
 # Boxplots with two grouping
 bwplot(Segment ~ income | ownHome, data = seg.df,
        horizontal = TRUE, xlab = "Income")
+
+# Statistical Tests
+#----------------------------------------------------------------------
+
+# Chi-Square Test - Tests equality of marginal counts in groups
+table(seg.df$subscribe, seg.df$ownHome)
+# Do they have the same rate of subscription by ownership of home
+chisq.test(table(seg.df$subscribe, seg.df$ownHome))
+
+# Proportions: binomial test - whether the count matches expected proportion
+binom.test(12, 20, p = 0.5)
+
+# T-tests: Compares the means of two groups
+# Null hypothesis: Two groups are different
+bwplot(income ~ ownHome, data = seg.df)
+t.test(income ~ ownHome, data = seg.df)
+# T-test for a subset of data
+t.test(income ~ ownHome, data = subset(seg.df, Segment == "Travelers"))
+# For travelers, p value > 0.05 so we fail to reject null hypothesis
+
+# ANOVA Basics: to compare more than 2 groups
+# For two groups it is effectively the same
+aov.inc.seg <- aov(income ~ Segment, data = seg.df)
+anova(aov.inc.seg)
+
+# Means plot
+install.packages("multcomp")
+library(multcomp)
+aov.inc.seg <- aov(income ~ -1 + Segment, data = seg.df) # model w/o intercept
+by.seg <- glht(aov.inc.seg) # means and CIs
+par(mar = c(5, 10, 5, 5)) # margins, bot, left, top, right
+plot(by.seg, xlab = "Income", main = "Mean income by segment")
+
+# Compare medians - If distribution is far from being normal, median may be
+# more meaningful than mean
+install.packages("RVAideMemoire")
+library(RVAideMemoire)
+mood.medtest(income ~ Segment, data = seg.df)
+mood.medtest(income ~ ownHome, data = seg.df)
